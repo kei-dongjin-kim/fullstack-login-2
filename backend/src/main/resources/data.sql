e250df863e3a91094e7f150fbd5805df7ec4003f
kei.dongjin.kim <kei.dongjin.kim@gmail.com>
2024-08-27 17:17:57 +0900

    Added Admin2 which has 2 roles

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,16 +2,17 @@
 INSERT INTO
   member (username, password, email, created_at)
 VALUES
-  ('Admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin@example.com', CURRENT_TIMESTAMP),
-  ('User', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user@example.com', CURRENT_TIMESTAMP)
+  ('Admin1', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin1@example.com', CURRENT_TIMESTAMP),
+  ('User1', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user1@example.com', CURRENT_TIMESTAMP),
+  ('Admin2', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin2@example.com', CURRENT_TIMESTAMP)
 ;
 
 -- Insert sample data into role table
 INSERT INTO
   role (name)
 VALUES
-  ('admin'),
-  ('user')
+  ('ADMIN'),
+  ('USER')
 ;
 
 -- Insert sample data into member_role table
@@ -19,5 +20,7 @@ INSERT INTO
   member_role (member_id, role_id)
 VALUES
   (1, 1),
-  (2, 2)
+  (2, 2),
+  (3, 1),
+  (3, 2)
 ;
